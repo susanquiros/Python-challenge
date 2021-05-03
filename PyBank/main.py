d00d9b4a396e1c648f6c862f1b32f6dccc28a9fe
Susan Quiros <squiros@houstonfoodbank.org>
2021-05-03 17:56:19 -0500

    getting the months and total amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,29 +14,23 @@ with open(csvpath) as csvfile:
 
       #variables
     count_months = 0 
+    total_amount = []
+    sum_total_amount = 0
 
     for row in csvreader:
         print (row)
         count_months = count_months + 1
+        total_amount.append(int(row[1]))
+        sum_total_amount= sum(total_amount) 
     #knowing what type of data it's my column 1    
-    print(type (row [1]))
-
-#
-
-  
-
-
-
-
-
-
-
-
-    
+        #print(type (row [1]))
+ 
 print ("Financial Analysis")
 print ("------------------")   
 print(f"Total Months: {count_months}")
+print(f'total:{sum_total_amount}')
 print("average change")
-print("Greates Increase in Profits")
-print("Greates Decrease in Profits")
+print("Greatest Increase in Profits")
+print("Greatest Decrease in Profits")
+#print(total_amount)
     
