117b1805ef3434ff069fd48b3ead27c008226eb2
Susan Quiros <squiros@houstonfoodbank.org>
2021-05-06 21:01:26 -0500

    getting the winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,8 @@ with open (csvpath) as csvfile:
     candidates= []
     unique_candidates = []
     votes = []
+    percentage = 0
+    percentage_votes = []
 
         
     for row in csvreader:
@@ -31,8 +33,14 @@ with open (csvpath) as csvfile:
       #counting the total votes by candidate
         count_votes = candidates.count(x)
         votes.append(count_votes)
-print(votes)
+        percentage = ((count_votes/Total_votes)*100)
+        percentage_votes.append(percentage)
+max_votes = max(votes)    
+winner = unique_candidates[votes.index(max_votes)] 
+print(winner)  
 
-    
+print(percentage_votes)
 print(Total_votes) 
-print(unique_candidates)          
+print(unique_candidates)
+print(votes)  
+          
